3d8ae2c8fbf15a53671ce2e0dae1d04c161a3d4a
Travis Courtney <travis@angry-fly.com>
2023-01-26 14:26:13 -0500

    Added notifications for the comments with noticed gem

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,19 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :set_notifications, if: :current_user
 
- protected
+  protected
 
- def configure_permitted_parameters
+  def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
     devise_parameter_sanitizer.permit(:account_update, keys: [:name])
- end
+  end
+
+  private
+
+  def set_notifications
+    notificiations = Notification.where(recipient: current_user).newest_first.limit(9)
+    @unread = notificiations.unread
+    @read = notificiations.read
+  end
 end
